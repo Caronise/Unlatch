a79f5a98d0fa3a7875350be201021305f30a6167
Slav <slalev57@me.com>
2020-01-06 18:09:25 -0500

    updated the database again

--- backend/db/schema/02_vehicles.sql
+++ backend/db/schema/02_vehicles.sql
@@ -4,10 +4,9 @@ DROP TABLE IF EXISTS vehicles CASCADE;
 CREATE TABLE vehicles ( 
   id SERIAL PRIMARY KEY NOT NULL, 
   user_id INTEGER REFERENCES users (id) ON DELETE CASCADE, 
+  make_id INTEGER REFERENCES makes (id) ON DELETE CASCADE,
   vehicle_name VARCHAR(255) NOT NULL,  
-  make VARCHAR(255) NOT NULL, 
-  model VARCHAR(255) NOT NULL, 
   year SMALLINT, 
   vin  VARCHAR(17),
-  picture_url VARCHAR(255)
+  picture_url VARCHAR(255) 
 );
