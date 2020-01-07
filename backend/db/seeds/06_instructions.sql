62a764f0640f59a9dc1682d7693861344f0b005b
Slav <slalev57@me.com>
2020-01-06 20:26:48 -0500

    created seeds for the tables 01 to 05 are done

---
+++ backend/db/seeds/06_instructions.sql
@@ -0,0 +1,25 @@
+-- Adding data to the instructions table 
+
+--Dodge Oil
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('1', '1' );
+
+--Dodge Brakes
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('2', '2' );
+
+--VW Oil
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('3', '3' );
+
+--VW Brakes
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('4', '4' );
+
+--Volvo Oil
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('5', '5' );
+
+--Volvo Brakes
+INSERT INTO instructions (id, project_id, steps)
+VALUES ('6', '6' );
