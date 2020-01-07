7660674b75cbbd25c3206d6239ed9702b1b6c900
Russell Cape <russellcape98@gmail.com>
2020-01-07 18:09:22 -0500

    Edited minor errors in the schema (db working)

--- backend/db/schema/09_custom_projects.sql
+++ backend/db/schema/09_custom_projects.sql
@@ -1,7 +1,7 @@
 -- Creating the custom_projetcs table 
 
-DROP TABLE IF EXISTS custom_projetcs CASCADE;
-CREATE TABLE custom_projetcs ( 
+DROP TABLE IF EXISTS custom_projects CASCADE;
+CREATE TABLE custom_projects ( 
   id SERIAL PRIMARY KEY NOT NULL, 
   premade_project_id INTEGER REFERENCES premade_projects (id) ON DELETE CASCADE, 
   custom_name VARCHAR(255) NOT NULL
