ba49864cfad4e2e65ca0a2cc2c73f3adec2fe908
Slav <slalev57@me.com>
2020-01-08 18:11:19 -0500

    added basic project component

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -2,15 +2,25 @@ import React from 'react'
 
 
 function Project () { 
-  return( <div className='top-bar'>
-           <span>Logo</span>
-           <div className='sign-in'>
-             <button>Sign up</button>
-             <button>Login</button>
-             <button>Logout</button>
-           </div>
-         </div> 
-         );  
+  return( 
+    <div className='info'>
+      <button className='show_info'> Show Info</button>
+      {/* Name of the project goes here */}
+      Project name 
+      <div className='parts'>Parts
+      {/* Parts name, price, part_number etc go here */}
+      </div>
+      <div className='instructions'> Instructions 
+      {/* The instructions go in here for the repair, steps etc */}
+      </div>
+      <div className='video'> Video
+      {/* Your video/s goes here */}
+      </div>
+      <div className='notes'> Notes
+      {/* Your notes go in here  */}
+      </div>
+    </div>
+  );  
 }
 
 export default Project; 
