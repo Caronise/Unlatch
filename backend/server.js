f0ef0a7266b035bdb06337488d8df785fc2d4b03
Russell Cape <russellcape98@gmail.com>
2020-01-08 18:09:53 +0000

    Replaced development user with labber

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Web server config
 const PORT       = process.env.PORT || 8000;
-const ENV        = process.env.ENV || "development";
+const ENV        = process.env.ENV || "labber";
 const express    = require("express");
 const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
