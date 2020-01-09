6583c56cb2487a54cae393eba0c097448eb2976a
Russell Cape <russellcape98@gmail.com>
2020-01-09 18:38:54 -0500

    Proxy issue (db:reset was not working) fixed

--- backend/db/seeds/03_vehicles.sql
+++ backend/db/seeds/03_vehicles.sql
@@ -1,15 +1,5 @@
 -- Adding vehicules to our vehicles table 
 
-<<<<<<< Updated upstream
-INSERT INTO vehicles (id, user_id, make_id, vehicle_name, year, vin, picture_url)
-VALUES ('1', '1', '4', 'Excaliber', '2010', '', '');
-
-INSERT INTO vehicles (id, user_id, make_id, vehicle_name, year, vin, picture_url)
-VALUES ('2', '1', '13', 'Daisy', '2013', '', ''); 
-
-INSERT INTO vehicles (id, user_id, make_id, vehicle_name, year, vin, picture_url)
-VALUES ('3', '1', '14', 'Carmobile', '2005', '', '');
-=======
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
 VALUES ('1', '4', '55', '2010', '');
 
@@ -18,4 +8,3 @@ VALUES ('1', '13', '162', '2013', '');
 
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
 VALUES ('1', '14', '172', '2005', '');
->>>>>>> Stashed changes
