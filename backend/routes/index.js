7c382d0e3f347e314d4962db88fb16de651b707c
Russell Cape <russellcape98@gmail.com>
2020-01-09 14:01:28 -0500

    routing work

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { getMakes, makesCheck } = require('../helpers/add_vehicle');
 
 module.exports = db => {
   
@@ -9,66 +10,75 @@ module.exports = db => {
   
   });
 
-  /* GET Login. */
-  router.get('/login', (req, res) => {
-    res.send('This is the login route')
+  // /* GET Login. */
+  // router.get('/login', (req, res) => {
+  //   res.send('This is the login route')
 
-  });
+  // });
 
-  /* POST Login. */
-  router.post('/login', (req, res) => {
-    const { email, password} = req.body;
-    const text = `
-      SELECT * FROM users
-      WHERE email = $1 AND password = $2
-    ;`;
-    const values = [ email, password ];
+  // /* POST Login. */
+  // router.post('/login', (req, res) => {
+  //   const { email, password} = req.body;
+  //   const text = `
+  //     SELECT * FROM users
+  //     WHERE email = $1 AND password = $2
+  //   ;`;
+  //   const values = [ email, password ];
 
-    db.query(text, values)
-      .then(data => {
-        if (data.rows[0].length === 0) {
-          res.send( { message: "You're not logged in!" });
-        } 
-        // else {
-        //   req.session.user_id = data.rows[0].id;
-        //   res.send( { message: "Succesfully set session" })
-        // }
-      })
-      .catch(error => {
-        console.log(`${error}`)
-      })
-  });
+  //   db.query(text, values)
+  //     .then(data => {
+  //       if (data.rows[0].length === 0) {
+  //         res.send( { message: "You're not logged in!" });
+  //       } 
+  //       // else {
+  //       //   req.session.user_id = data.rows[0].id;
+  //       //   res.send( { message: "Succesfully set session" })
+  //       // }
+  //     })
+  //     .catch(error => {
+  //       console.log(`${error}`)
+  //     })
+  // });
 
-  /* GET Register. */
-  router.get('/register', (req, res) => {
-    res.send('This is the register route')
-  });
+  // /* GET Register. */
+  // router.get('/register', (req, res) => {
+  //   res.send('This is the register route')
+  // });
 
-  /* POST Register. */
-  router.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-    const text = `
-      INSERT INTO users (username, email, password)
-      VALUES ($1, $2, $3)
-      RETURNING id
-    ;`;
-    const values = [username, email, password];
+  // /* POST Register. */
+  // router.post('/register', (req, res) => {
+  //   const { username, email, password } = req.body;
+  //   const text = `
+  //     INSERT INTO users (username, email, password)
+  //     VALUES ($1, $2, $3)
+  //     RETURNING id
+  //   ;`;
+  //   const values = [username, email, password];
 
-    db.query(text, values)
-      .then(data => {
-        const userId = data.rows[0].id;
-        // req.session.user_id = userId
-        res.send( {username} );
-      })
-      .catch(error => {
-        console.log(`${error}`)
-      });
-  });
+  //   db.query(text, values)
+  //     .then(data => {
+  //       const userId = data.rows[0].id;
+  //       // req.session.user_id = userId
+  //       res.send( {username} );
+  //     })
+  //     .catch(error => {
+  //       console.log(`${error}`)
+  //     });
+  // });
 
-  /* GET Logout. */
-  router.get('/logout', (req, res) => {
-    res.send('This is the logout route')
-  });
+  // /* GET Logout. */
+  // router.get('/logout', (req, res) => {
+  //   res.send('This is the logout route')
+  // });
+
+  // router.post('/logout', (req, res) => {
+  //   req.session.id = null;
+  //   res.send({});
+  // });
+
+  // router.get('/vehicles', (req, res) => {
+  //   res.send('This is the add vehicle route')
+  // })
 
   /* POST Add Vehicle. */
   router.post('/vehicles', (req, res) => {
@@ -105,9 +115,31 @@ module.exports = db => {
           .then(data => {
             // - insert the todo in the database with the category
             const text = `
-            INSERT INTO vehicles ()
+            INSERT INTO vehicles (id, user_id, make_id, vehicle_name, year, vin, picture_url)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING *
             `;
+            const values = [1, data.rows[0].id, make_id, vehicle_name, year, vin, picture_url];
+            db.query(text, values)
+              .then(data => {
+              // send back response to the client (response is the new vehicle, with make id)
+              return (data.rows[0]);
+              })
+              .then(newVehicle => {
+                const query = {
+                  text: 'SELECT vehicle_name FROM categories WHERE id = $1',
+                  values: [newVehicle.vehicle_name]
+                }
+                db.query(query)
+                  .then(result=>{
+                  newTodo.category = result.rows[0].vehicle_name;
+                  res.json(newVehicle);
+                })
+              })
           })
+          .catch(error => {
+            console.log(`${error}`);
+          });
       });
   });
     
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,76 @@
-var express = require('express');
-var router = express.Router();
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
+module.exports = function(router, database) {
 
-module.exports = db => {
+  // Create a new user
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    database.addUser(user)
+    .then(user => {
+      if (!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send("🤗");
+    })
+    .catch(e => res.send(e));
+  });
 
-  /* GET Login. */
-  router.get('/login', (req, res) => {
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login =  function(email, password) {
+    return database.getUserWithEmail(email)
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  }
+  exports.login = login;
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({user: {name: user.name, email: user.email, id: user.id}});
+      })
+      .catch(e => res.send(e));
+  });
+  
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+  router.get("/me", (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send({message: "not logged in"});
+      return;
+    }
+
+    database.getUserWithId(userId)
+      .then(user => {
+        if (!user) {
+          res.send({error: "no user with that id"});
+          return;
+        }
     
+        res.send({user: {name: user.name, email: user.email, id: userId}});
+      })
+      .catch(e => res.send(e));
   });
 
-  /* GET Register. */
-  router.get('/register', (req, res) => {
-    res.send('This is the register route')
-  });
-
-  /* GET Logout. */
-  router.get('/logout', (req, res) => {
-    res.send('This is the logout route')
-  });
-
-  return router
-};
+  return router;
+}
