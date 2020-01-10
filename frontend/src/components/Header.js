9b925c5c23b02411a17dfbb4fa6c8c0f871c6502
Russell Cape <russellcape98@gmail.com>
2020-01-10 11:00:25 -0500

    Removed span

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,6 @@ function Header() {
   return (
     <div className='top-bar'>
       <img src="images/logo.png" width="250" height="75"></img>
-      {/* <span>Logo</span> */}
       <div className='dropdown-wrapper'>
         <div className="dropdown-header">
           <button>Username's Garage</button>
