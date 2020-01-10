9a895298ede45e97b2b2999263e98f2d53b032f0
Caronise <lucas.betancourt@hotmail.com>
2020-01-10 12:57:20 -0500

    Fixed a mix up between projects / selected_project

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,13 +4,8 @@ import React from 'react'
 function Header() {
   return (
     <div className='top-bar'>
-<<<<<<< HEAD
-      <img src="images/logo.png" width="250" height="75"></img>
-=======
       <img src="images/logo.png" alt="Logo" width="250" height="75"></img>
-      {/* <span>Logo</span> */}
 
->>>>>>> 0d1b8b1bdba2f53865b05ebec4f2784fb3d26f13
       <div className='dropdown-wrapper'>
         <div className="dropdown-header">
           <button>Username's Garage</button>
--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -3,23 +3,10 @@ import React from 'react'
 
 function Projects () { 
   return( 
-    <div className='projects'>
-      <button className='show_info'> Show Info</button>
-      <br />
-      {/* Name of the project goes here */}
-      <h2>Project Name</h2> 
-      <div className='project_parts'>Parts
-      {/* Parts name, price, part_number etc go here */}
-      </div>
-      <div className='project_instructions'> Instructions 
-      {/* The instructions go in here for the repair, steps etc */}
-      </div>
-      <div className='project_video'> Video
-      {/* Your video/s goes here */}
-      </div>
-      <div className='project_notes'> Notes
-      {/* Your notes go in here  */}
-      </div>
+    <div className='repair-options'>
+      <h5>What would you like to do?</h5>
+      <p className='odd'>Project One</p>
+      <p className='even'>Project Two</p>
     </div>
   );  
 }
