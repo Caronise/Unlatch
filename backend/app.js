720e46aefb663f1426fc61efd3378ea359fda7f8
Russell Cape <russellcape98@gmail.com>
2020-01-10 12:41:44 -0500

    Minor refactor/ commented out a unused require

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const { Client } = require('pg');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 const cors = require('cors');
 require('dotenv').config();
 
-var app = express();
+const app = express();
 
 
 const connectionOptions = {
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(cors());
 
