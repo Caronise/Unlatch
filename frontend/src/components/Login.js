403f9e02d73ea7ff05ab92675ee1df137ea85f98
Russell Cape <russellcape98@gmail.com>
2020-01-10 16:11:52 -0500

    added bcrypt and cookie sessions and working on login and register states

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login(props) {
 
-  debugger;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const authenticate = () => {
+    
+
+  
+  };
 
   return (
     <div className="login">
+      <form id="login-form" action="/login" method="POST" onSubmit={authenticate}>
       <p>Email</p>
-      <textarea id="email_verify" name="email_verify" rows="1" cols="25"> </textarea>
+      <input id="email_verify" name="email_verify" type="email" placeholder="example@email.com" value={email} onChange={event => setEmail(event.target.value)}/>
       <p>Password</p>
-      <textarea id="password_verify" name="password_verify" input type="password" rows="1" cols="25"> </textarea>
-      <br />
+      <input id="password_verify" name="password_verify" type="password" placeholder="**********" value={password} onChange={event => setPassword(event.target.value)}/>
       <button className="login_btn" action="/login" method="GET">Login</button>
+      </form>
       <button className="login_back_btn" action="/landing" method="GET">Back</button>
     </div>
   );
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function RegisterPage() {
+
+const [user, setUser] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
   return (
     <div className='register'>
       <p>Username</p>
-       <textarea id="username" name="username" rows="1" cols="25"> </textarea>
+       <input id="username" name="username" rows="1" cols="25" value={user} onChange={event => setUser(event.target.value)}/>
       <p>Email</p>
-       <textarea id="email" name="email" rows="1" cols="25"> </textarea>
+       <input id="email" name="email" rows="1" cols="25" value={email} onChange={event => setEmail(event.target.value)}/>
       <p>Password</p>
-       <textarea id="password" name="password" rows="1" cols="25"> </textarea>
+       <input id="password" name="password" rows="1" cols="25" value={password} onChange={event => setPassword(event.target.value)}/>
        <br />
       <button className="register_btn" action='/register' method='POST'>Register</button>
       <button className="register_back_btn" action="/landing" method="POST">Back</button>
