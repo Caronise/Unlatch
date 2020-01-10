c3fb4a76780b61679ee1693b7a01d2b776d20bf7
Caronise <lucas.betancourt@hotmail.com>
2020-01-09 20:30:16 -0500

    fixed the merging conflict

--- backend/db/seeds/03_vehicles.sql
+++ backend/db/seeds/03_vehicles.sql
@@ -7,9 +7,5 @@ INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
 VALUES ('1', '13', '162', '2013', ''); 
 
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-<<<<<<< HEAD
-VALUES ('1', '14', '172', '2005', '');
-=======
 VALUES ('1', '14', '142', '2005', '');
 
->>>>>>> ee26709e4295fd4fbd38d983aa8bc83b337c09a0
