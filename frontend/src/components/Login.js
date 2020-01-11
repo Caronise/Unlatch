13fdb982deba70b328902a582c736239fba8396f
Russell Cape <russellcape98@gmail.com>
2020-01-11 11:17:11 -0500

    Register page adds user to database (need to make conditions for 3 data fields)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -13,7 +13,6 @@ function Login() {
 
     axios.post('/login', {email, password})
       .then((result) => {
-        console.log(result)
         console.log(result.data)
       })
       .catch(err => console.log(err))
@@ -22,11 +21,11 @@ function Login() {
   return (
     <div className="login">
       <form id="login-form" onSubmit={authenticate}>
-      <p>Email</p>
-      <input id="email" name="email" type="email" placeholder="example@email.com" value={email} onChange={event => setEmail(event.target.value)}/>
-      <p>Password</p>
-      <input id="password" name="password" type="password" placeholder="**********" value={password} onChange={event => setPassword(event.target.value)}/>
-      <input type="submit" value="Submit"/>
+        <p>Email</p>
+        <input id="email" name="email" type="email" placeholder="example@email.com" value={email} onChange={event => setEmail(event.target.value)}/>
+        <p>Password</p>
+        <input id="password" name="password" type="password" placeholder="**********" value={password} onChange={event => setPassword(event.target.value)}/>
+        <input type="submit" value="Submit"/>
       </form>
       <button className="login_back_btn">Back</button>
     </div>
