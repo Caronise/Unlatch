383627f17e9e77154e33bb4b513df168004a988a
Russell Cape <russellcape98@gmail.com>
2020-01-13 15:59:18 -0500

    Small refactor edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+
 // import axios from 'axios';
 import './App.css';
 import newCar from './images/charger.jpg';
--- frontend/src/components/AddVehicle.js
+++ frontend/src/components/AddVehicle.js
@@ -5,14 +5,16 @@ function AddVehicle() {
   return (
     <div className="add_vehicle">
       <h3>Please enter the vehicle information</h3>
+      <form id="add_vehicle_form">
       <p>Make</p>
-       <textarea id="make" name="make" rows="1" cols="25"> </textarea>
+       <input id="make" name="make" rows="1" cols="25"/>
       <p>Model</p>
-       <textarea id="model" name="model" rows="1" cols="25"> </textarea>
+       <input id="model" name="model" rows="1" cols="25"/>
       <p>Year</p>
-       <textarea id="year" name="year" rows="1" cols="25"> </textarea>
+       <input id="year" name="year" rows="1" cols="25"/>
        <br />
       <button className="add_vehicle_btn" action="/vehicle" method="POST">Submit</button>
+      </form>
       <button className="add_vehicle_back_btn" action="/garage" method="GET">Back</button>
     </div>
   );
