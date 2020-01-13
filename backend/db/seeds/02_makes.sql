3ab6e3f13fcaf52e222da780ea29d5f958549bbd
Russell Cape <russellcape98@gmail.com>
2020-01-13 11:25:49 -0500

    Reconfigured DB

--- backend/db/seeds/02_makes.sql
+++ backend/db/seeds/02_makes.sql
@@ -1,57 +1,57 @@
 --Adding makes to the makes tables 
 
-INSERT INTO makes (make_name)
-VALUES ('Audi'); 
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Audi'); 
 
 
-INSERT INTO makes (make_name)
-VALUES ('BMW');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'BMW');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Chevrolet');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Chevrolet');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Dodge');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Dodge');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Ford');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Ford');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Honda');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Honda');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Hyundai');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Hyundai');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Lexus');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Lexus');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Mazda');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Mazda');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Mercedes-Benz');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Mercedes-Benz');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Nissan');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Nissan');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Toyota');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Toyota');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Volkswagen');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Volkswagen');
 
 
-INSERT INTO makes (make_name)
-VALUES ('Volvo');
+INSERT INTO makes (user_id, make_name)
+VALUES ('1', 'Volvo');
 
--- backend/db/seeds/04_vehicles.sql
+++ backend/db/seeds/04_vehicles.sql
@@ -1,11 +1,11 @@
 -- Adding vehicules to our vehicles table 
 
-INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '4', '53', '2010', '');
+INSERT INTO vehicles (model_id, year, picture_url)
+VALUES ('53', '2010', '');
 
-INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '13', '142', '2013', ''); 
+INSERT INTO vehicles (model_id, year, picture_url)
+VALUES ('142', '2013', ''); 
 
-INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '14', '157', '2005', '');
+INSERT INTO vehicles (model_id, year, picture_url)
+VALUES ('157', '2005', '');
 
