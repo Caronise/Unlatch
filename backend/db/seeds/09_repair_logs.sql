af1cebf0d006fbfc0e57a4b9699fbb5092c474cf
Russell Cape <russellcape98@gmail.com>
2020-01-15 17:52:05 -0500

    Added more seeds to repair log and did styling

--- backend/db/seeds/09_repair_logs.sql
+++ backend/db/seeds/09_repair_logs.sql
@@ -11,3 +11,27 @@ VALUES ('2', 'Steering wheel leans left by itself', '165000', '2019-06-25 19:10:
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
 VALUES ('2', 'Replaced left mirror', '170000', '2019-06-25 19:10:25-07', '89.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('3', 'Noticed that break fluid is running low', '150000', '2019-06-22 06:27:28-07', '20.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('3', 'Changed front wipers', '155000', '2019-06-22 06:27:28-07', '54.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('4', 'Steering wheel leans left by itself', '165000', '2019-06-25 19:10:25-07', '59.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('4', 'Replaced left mirror', '170000', '2019-06-25 19:10:25-07', '89.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('5', 'Noticed that break fluid is running low', '150000', '2019-06-22 06:27:28-07', '20.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('5', 'Changed front wipers', '155000', '2019-06-22 06:27:28-07', '54.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('6', 'Steering wheel leans left by itself', '165000', '2019-06-25 19:10:25-07', '59.99');
+
+INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
+VALUES ('6', 'Replaced left mirror', '170000', '2019-06-25 19:10:25-07', '89.99');
