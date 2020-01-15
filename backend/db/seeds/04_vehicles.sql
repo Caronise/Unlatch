50d3dedb5f9d11a51cdeb5ea72e4e29a1bec3556
Russell Cape <russellcape98@gmail.com>
2020-01-15 15:09:45 -0500

    Added vehicle images

--- backend/db/seeds/04_vehicles.sql
+++ backend/db/seeds/04_vehicles.sql
@@ -1,11 +1,11 @@
 -- Adding vehicules to our vehicles table 
 
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '4', '52', '2010', '');
+VALUES ('1', '4', '52', '2010', 'https://static.cargurus.com/images/site/2010/01/19/17/36/2010_dodge_caliber-pic-4486811832233687058-640x480.jpeg');
 
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '13', '148', '2013', ''); 
+VALUES ('1', '13', '148', '2013', 'https://shinegraffix.com/wp-content/uploads/2017/01/gti-mk6-kit4.jpg'); 
 
 INSERT INTO vehicles (user_id, make_id, model_id, year, picture_url)
-VALUES ('1', '14', '162', '2005', '');
+VALUES ('1', '14', '162', '2005', 'http://img.autoabc.lv/volvo-s40/volvo-s40_2004_Sedans_15101383731.jpg');
 
