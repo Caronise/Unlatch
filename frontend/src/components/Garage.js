0ee6e74029815f8b73f7280d2a072560b9e0a6c3
Caronise <lucas.betancourt@hotmail.com>
2020-01-14 20:29:19 -0500

    Pages now flow normally after selecting project and vehicle. Video is embeded

--- frontend/src/components/Garage.js
+++ frontend/src/components/Garage.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 import {
   Form
 } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 function Garage({ setCurrentVehicle, setProjects }) {
+  let history = useHistory();
   const user = useContext(UserContext);
   const vehicles = useContext(VehiclesContext);
 
@@ -20,8 +22,9 @@ function Garage({ setCurrentVehicle, setProjects }) {
 
     axios.get(`/vehicles/${chosenVehicle.vehicle_id}/projects`)
       .then(res => {
-        console.log("Data from projects call: ", res.data)
-        setProjects(res.data)
+        console.log("Data from projects call: ", res.data);
+        setProjects(res.data);
+        history.push('/garage/{chosenVehicle.vehicle_id}');
       })
   };
 
@@ -34,11 +37,11 @@ function Garage({ setCurrentVehicle, setProjects }) {
       <label>Choose a vehicle:</label>
       <Form.Group controlId="exampleForm.ControlSelect1">
         <Form.Label>--Please choose your vehicle--</Form.Label>
-        <Form.Control as="select" name="vehicle_select" id="vehicle_select">
+        <Form.Control as="select" name="vehicle_select">
 
         {vehicles.map((vehicle) => 
-        <option value={vehicle.vehicle_id} onClick={handleSelection}>{vehicle.make_name} {vehicle.model_name}</option>)
-        }
+        <option key={vehicle.vehicle_id} value={vehicle.vehicle_id} onClick={handleSelection}>{vehicle.make_name} {vehicle.model_name}</option>
+        )}
           
         </Form.Control>
       </Form.Group>
--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../helpers/UserContext';
+import { useHistory } from 'react-router-dom';
 
 function Projects({ projects, setCurrentProject}) {
+  let history = useHistory();
   const user = useContext(UserContext);
 
   const handleSelection = (event) => {
@@ -12,6 +14,7 @@ function Projects({ projects, setCurrentProject}) {
     console.group("EVENT VALUE: ", event.target.value);
 
     setCurrentProject(chosenProject);
+    history.push('/projects/{chosenProject.id}');
   };
 
   return (
@@ -22,7 +25,7 @@ function Projects({ projects, setCurrentProject}) {
         <label>Choose a project:</label>
         <select name="vehicle_select" id="vehicle_select">
           <option value="">--Please select a project--</option>
-          {projects.map(project => <option value={project.id} onClick={handleSelection}> Project: {project.project_name} </option>)}
+          {projects.map(project => <option key={project.id} value={project.id} onClick={handleSelection}> Project: {project.project_name} </option>)}
         </select>
 
         <br />
