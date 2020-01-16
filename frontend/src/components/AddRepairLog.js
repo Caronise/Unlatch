2ec84f34914d7e5a551ac17426c2fd465224483f
Russell Cape <russellcape98@gmail.com>
2020-01-16 14:39:24 -0500

    Restyled addRepairLog page

--- frontend/src/components/AddRepairLog.js
+++ frontend/src/components/AddRepairLog.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
 
 function AddRepairLog({ currentProject }) {
@@ -26,18 +27,33 @@ function AddRepairLog({ currentProject }) {
 
   return (
     <div className="add_repair_log">
-      <h3>Add a repair log</h3>
-      <p>Repair Description: </p>
-      <textarea id="description" name="description" rows="1" cols="25" value={description} onChange={event => setDescription(event.target.value)}> </textarea>
-      <p>Mileage: </p>
-      <textarea id="mileage" name="mileage" rows="1" cols="25" value={mileage} onChange={event => setMileage(event.target.value)}> </textarea>
-      <p>Date of repair: </p>
-      <textarea id="timestamp" name="timestamp" rows="1" cols="25" value={timestamp} onChange={event => setTimestamp(event.target.value)}> </textarea>
-      <p>Cost of repair: </p>
-      <textarea id="cost_of_repair" name="cost_of_repair" rows="1" cols="25" value={cost_of_repair} onChange={event => setCost_of_repair(event.target.value)}> </textarea>
-      <br />
-      <button className="add_repair_back_btn" onClick={() => history.push(`/projects/${currentProject.id}`)}>Back</button>
-      <button className="add_repair_btn" onClick={submitRepairLog}>Submit</button>
+      <h3 className="repair_log_title">Add a repair log</h3>
+      <Form>
+        <Form.Group className="add_log_forms">
+          <Form.Label className="add_label">Repair Description:</Form.Label>
+          <Form.Control type="text" id="description" name="description" placeholder="Description" value={description} onChange={event => setDescription(event.target.value)}/>
+        </ Form.Group>
+        <Form.Group className="add_log_forms">
+          <Form.Label className="add_label">Mileage:</Form.Label>
+          <Form.Control placeholder="Mileage" type="text" id="mileage" name="mileage" value={mileage} onChange={event => setMileage(event.target.value)}/>
+        </ Form.Group>
+        <Form.Group className="add_log_forms">
+          <Form.Label className="add_label">Date of Repair:</Form.Label>
+          <Form.Control placeholder="Date of Repair" type="text" id="timestamp" name="timestamp" value={timestamp} onChange={event => setTimestamp(event.target.value)}/>
+        </ Form.Group>
+        <Form.Group className="add_log_forms">
+          <Form.Label className="add_label">Cost of repair: </Form.Label>
+          <Form.Control placeholder="Cost of Repair" type="text" id="cost_of_repair" name="cost_of_repair" value={cost_of_repair} onChange={event => setCost_of_repair(event.target.value)}/>
+        </ Form.Group>
+        <Form.Group className="add_and_back">
+          <Button variant="warning" className="add_repair_back_btn" onClick={() => history.push(`/projects/${currentProject.id}`)}>
+            Back
+          </Button>
+          <Button variant="success" className="add_repair_btn" onClick={submitRepairLog}>
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
