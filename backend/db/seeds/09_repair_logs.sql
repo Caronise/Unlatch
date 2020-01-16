910efde9c8fe1d19260060b910240c9de6ecd71d
Caronise <lucas.betancourt@hotmail.com>
2020-01-16 15:10:19 -0500

    Modified project seeds

--- backend/db/seeds/09_repair_logs.sql
+++ backend/db/seeds/09_repair_logs.sql
@@ -1,10 +1,10 @@
 --Adding notes to the notes table
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
-VALUES ('1', 'Noticed that break fluid is running low', '150000', '2019-06-22 06:27:28-07', '20.99');
+VALUES ('1', 'Changed the oil', '80000', '2019-06-22 06:27:28-07', '49.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
-VALUES ('1', 'Changed front wipers', '155000', '2019-06-22 06:27:28-07', '54.99');
+VALUES ('1', 'Changed front wipers', '83200', '2019-08-01 06:27:28-07', '54.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
 VALUES ('2', 'Steering wheel leans left by itself', '165000', '2019-06-25 19:10:25-07', '59.99');
@@ -13,7 +13,7 @@ INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_re
 VALUES ('2', 'Replaced left mirror', '170000', '2019-06-25 19:10:25-07', '89.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
-VALUES ('3', 'Noticed that break fluid is running low', '150000', '2019-06-22 06:27:28-07', '20.99');
+VALUES ('3', 'Changed the oil', '100000', '2019-06-29 06:27:28-07', '42.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
 VALUES ('3', 'Changed front wipers', '155000', '2019-06-22 06:27:28-07', '54.99');
@@ -25,7 +25,7 @@ INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_re
 VALUES ('4', 'Replaced left mirror', '170000', '2019-06-25 19:10:25-07', '89.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
-VALUES ('5', 'Noticed that break fluid is running low', '150000', '2019-06-22 06:27:28-07', '20.99');
+VALUES ('5', 'Changed the oil', '75000', '2019-06-22 06:27:28-07', '20.99');
 
 INSERT INTO repair_logs (project_id, description, mileage, timestamp, cost_of_repair)
 VALUES ('5', 'Changed front wipers', '155000', '2019-06-22 06:27:28-07', '54.99');
