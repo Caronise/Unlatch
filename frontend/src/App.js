c647591557bd6b38e45a8bb68cbca973b9597488
Caronise <lucas.betancourt@hotmail.com>
2020-01-16 11:02:42 -0500

    Added Link in App.js to compile properly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 import axios from 'axios';
 import './App.css';
